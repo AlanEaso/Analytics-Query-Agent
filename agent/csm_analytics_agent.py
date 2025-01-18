ee2b0141c9c571f0fa3a48c15a3e98dcd96236e2
Alan <alaneasow@gmail.com>
2025-01-19 02:10:09 +0530

    Added agent to handle the initial response to check satisfaction of the user

--- agent/csm_analytics_agent.py
+++ agent/csm_analytics_agent.py
@@ -1,5 +1,10 @@
 import os
 import sys
+from agent.probing_agent import ProbingAgent
+from agent.memory import ConversationMemory
+import openai
+from llm.openai_client import OpenAiClient
+import json
 
 parent_dir = os.path.dirname(os.getcwd())
 sys.path.append(parent_dir)
@@ -19,16 +24,20 @@ class CSMAnalytsgent:
     def __init__(self):
         self.agent_status = AgentState.INITIALIZING
         self.knowledge_base = Report()
+        self.query = None
+        self.openai_client = OpenAiClient().client()
 
-    def process_query(self, query: str, top_k: int = 3):
+    def process_query(self, memory: ConversationMemory, query: str, top_k: int = 3):
         self.agent_status = AgentState.SEARCHING
-        return self.search_reports(query=query, top_k=top_k)
+        self.query = query
+        memory.add_content(role='user', query=query)
+        return self.search_reports(top_k=top_k)
 
     def set_probing_state(self):
         self.agent_status = AgentState.PROBING
 
-    def search_reports(self, query: str, top_k: int = 3):
-        results = self.knowledge_base.search_reports(query, top_k)
+    def search_reports(self, top_k: int = 3):
+        results = self.knowledge_base.search_reports(self.query, top_k)
         for report, score in results:
             print(f"Title: {report.title}")
             print(f"Description: {report.description}")
@@ -44,5 +53,35 @@ class CSMAnalytsgent:
             return
         
         print("Probing...")
-        # Code to probe the user for more information
+        # probing_agent = ProbingAgent()
+        # probing_agent.probe_user("")
+
+    def handle_initial_satisfaction(self, user_satisfaction_query: str):
+        messages = [{
+            "role": "system",
+            "content": """ You are an Analytics Query Agent. Based on the knowledge you have, you provided and anwer
+             to the user's query. The user is now asked if they are satisfied with the results. If the user is satisfied,
+             return the response in the format: {user_satisfaction_response: "yes"}. If the user is not satisfied, return
+             the response in the following json format: {user_satisfaction_response: "no"} . Do not deviate from the response format.
+             The values should always be "yes" or "no" """
+        },
+        {
+            "role": "user",
+            "content": user_satisfaction_query
+        }]
+
+        response = self.openai_client.chat.completions.create(
+            model="llama-3.3-70b-versatile",
+            messages=messages,
+            max_completion_tokens=1024,
+            temperature=0.5,
+            response_format={"type": "json_object"} 
+        )
+
+        json_response = json.loads(response.choices[0].message.content)
+        if json_response['user_satisfaction_response'] == "no":
+            self.agent_status = AgentState.PROBING
+        else:
+            self.agent_status = AgentState.COMPLETE
+        return json_response
         
--- cli.py
+++ cli.py
@@ -1,25 +1,32 @@
-from agent.csm_analytics_agent import CSMAnalytsgent
+from agent.csm_analytics_agent import CSMAnalytsgent, AgentState
+from agent.memory import ConversationMemory
 
 def main():
     agent = CSMAnalytsgent()
+    memory = ConversationMemory()
     print("CSM Analytics Query Agent")
     print("------------------------")
     print("Enter your analytics report request (or 'quit' to exit):")
+    try:
+        while True:
+            query = input("\nQuery: ").strip()
+            if query in ['quit', 'exit', 'q']:
+                break
 
-    while True:
-        query = input("\nQuery: ").strip()
-        if query in ['quit', 'exit', 'q']:
-            break
+            agent.process_query(memory= memory, query=query, top_k=1)
 
-        agent.process_query(query, top_k=1)
+            user_satisfaction_query = input("\n Are you satisfied with the results?: ").strip()
+            agent.handle_initial_satisfaction(user_satisfaction_query)
 
-        user_satisfaction_response = input("\n Are you satisfied with the results? (y/n): ").strip()
-        if user_satisfaction_response.lower() == 'n':
-            agent.set_probing_state()
-    
-        agent.process_probing()
+            if agent.agent_status == AgentState.COMPLETE:
+                print("\nGreat! Have a nice day!")
+                break
 
-        print("\n Enter any of the following commands or enter a new search query: 'quit', 'exit', 'q' to exit")
+            agent.process_probing()
+
+            print("\n Enter any of the following commands or enter a new search query: 'quit', 'exit', 'q' to exit")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
